de05d7b67850b7ab0bbfe1f6c501e0de835a38e3
Aryan Bhatt <abhatt30@students.cs.ubc.ca>
2026-04-29 14:15:39 -0600

    Find the sum of two constants

---
+++ basics/src/basics/SumTwoNumbers.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        final int num1 = 0; // initializing a constant num2 with the value 2
+        final int num2 = 2; // initializing a constant num2 with the value 2
+
+        int sum = num1 + num2; // calculates the sum of the numbers and stores the value in a variable sum
+
+        System.out.println(sum); // prints the sum of num1 and num2
+    } // end of main
+} // end of class
