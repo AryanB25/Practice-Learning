52978d54ad4de2335bd743cfa61693b764175c8e
Aryan Bhatt <abhatt30@students.cs.ubc.ca>
2026-05-03 19:12:43 -0600

    Variables and Arithmetics

---
+++ basics/src/basics/Variables.py
@@ -0,0 +1,28 @@
+# This program displays uses variables
+
+full_name = "Aryan Bhatt"
+age = 18
+gpa = 4.0
+is_Student = True
+
+print(f"Hello {full_name}!")  # f means format
+print(f"My age is {age}")
+print(f"My GPA is {gpa}")
+
+if is_Student:
+    print("You are a student!")
+else:
+    print("You are not a student")
+
+a = 2
+a += 2
+a += 3
+
+print(a)
+print(a - 7)
+
+b = 5
+print(a + b)
+
+c = 4
+print(c + a - b)
