18e6f5c78b20ae5c867dcbcc9234db12f02d7ce7
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-06-04 17:28:56 -0600

    BST (In-Order)

--- dsa/src/BSTInOrder.java
+++ dsa/src/BSTInOrder.java
@@ -1,8 +1,7 @@
 public class BSTInOrder {
-
-    // stores the structure of the node
+    // represents the structure of the node
     static class Node {
-        private int data; // stores the value of the node
+        private int data; // stores the data of the node
         private Node left; // stores the left node of the current node
         private Node right; // stores the right node of the current node
 
@@ -15,37 +14,34 @@ public class BSTInOrder {
     }
 
     static Node buildTree(Node root, int val) {
-        if (root == null) {
+        if (root == null) { // if the root is null or we reach a leaf of the BST
             root = new Node(val);
             return root;
         }
-
-        if (root.data > val) {
-            root.left = buildTree(root.left, val);
-        } else {
-            root.right = buildTree(root.right, val);
+        if (root.data > val) { // if the root's value is greater than the given value
+            root.left = buildTree(root.left, val); // the value will be on the left
+        } else { // if the root's value is lesser than the given value
+            root.right = buildTree(root.right, val); // the value will be on the right
         }
-
-        return root;
+        return root; // returns the root of the BST
     }
 
     static void inOrder(Node root) {
-        if (root == null)
+        if (root == null) { // if the root is null
             return;
+        }
         inOrder(root.left);
-        System.out.print(root.data + " ");
+        System.out.print(root.data + " "); // print the value of the root
         inOrder(root.right);
     }
 
     public static void main(String[] args) {
         int orders[] = { 5, 1, 3, 4, 2, 7 };
         Node root = null;
-
         for (int i = 0; i < orders.length; i++) {
-            root = buildTree(root, orders[i]);
+            root = buildTree(root, orders[i]); // create the BST
         }
-
-        inOrder(root);
+        inOrder(root); // print the nodes of the BST (In-Order)
         System.out.println();
     }
 }
