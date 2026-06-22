841e697f5b36e36f7cfdb633333594fed7d827fa
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-06-22 14:46:19 -0600

    Arrays

--- basics/bin/basics/Arrays.go
+++ basics/bin/basics/Arrays.go
@@ -7,4 +7,5 @@ func main() {
 	var names = [3]string{"ARYAN", "ALEX", "BRYAN"} // array that stores names
 	fmt.Println(a)                                  // print array "a"
 	fmt.Println(names)                              // print array "names"
+	fmt.Println(len(names))                         // print the number of names
 }
