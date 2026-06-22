0d1caa5845390aaa39845d1c63d0c0d7d4cfc2c1
Aryan Bhatt <aryandeepa2007@gmail.com>
2026-06-22 14:42:02 -0600

    Arrays

---
+++ basics/src/basics/Arrays.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [5]int{1, 2, 3}                            // array that stores 3 given numbers and 2 0's
+	var names = [3]string{"ARYAN", "ALEX", "BRYAN"} // array that stores names
+	fmt.Println(a)                                  // print array "a"
+	fmt.Println(names)                              // print array "names"
+}
